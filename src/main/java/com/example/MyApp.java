578018b6c6f30420c4a793314abbb688ed1cba45
sasnii <sasnii@users.noreply.github.com>
2020-01-11 16:13:47 +0100

    Completed1

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -8,8 +8,11 @@ public class MyApp {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
+        MessageService messageService2 = applicationContext.getBean("messageService", MessageService.class);
 
         System.out.println(messageService.getMessage());
+        System.out.println("Hash messageService : " + messageService.hashCode());
+        System.out.println("Hash messageService2 : " + messageService2.hashCode());
 
         applicationContext.close();
     }
--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -1,4 +1,22 @@
 package com.example;
 
-public class RandomTextMessageService {
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Component("randomMessageService")
+
+public class RandomTextMessageService implements MessageService {
+
+    List<String> lis1 = Arrays.asList( "Alex", "Brian", "Charles", "Jacob", "Suzie", "Melisa", "Matthew", "Michael", "Malyna", "Marisa ");
+    Random rand = new Random();
+
+
+    @Override
+    public String getMessage() {
+        return lis1.get(rand.nextInt(10));
+    }
 }
